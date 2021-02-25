7254c1d22dccd8e7c4b21fb155e6f8e95d419dd1
Mathis Logemann <mathisloge@gmail.com>
2021-02-25 17:06:32 +0100

    not working!

---
+++ src/plugin.cpp
@@ -0,0 +1,28 @@
+#include <Corrade/PluginManager/AbstractManager.h>
+#include <dt/df/plugin/plugin.hpp>
+#include <imnodes.h>
+
+namespace dt::df::plugin
+{
+
+class MyPlugin final : public Plugin
+{
+
+  public:
+    explicit MyPlugin(Corrade::PluginManager::AbstractManager &manager, const std::string &plugin)
+        : Plugin{manager, plugin}
+    {}
+
+    void setup(ImGuiContext *imgui_ctx, imnodes::Context *imnodes_ctx)
+    {
+        ImGui::SetCurrentContext(imgui_ctx);
+        imnodes::SetCurrentContext(imnodes_ctx);
+    }
+    void registerNodeFactories(IGraphManager &graph)
+    {}
+    void registerSlotFactories(IGraphManager &graph)
+    {}
+};
+} // namespace dt::df::plugin
+
+CORRADE_PLUGIN_REGISTER(MyPlugin, dt::df::plugin::MyPlugin, "de.mathisloge.dt.dataflow/1.0")
